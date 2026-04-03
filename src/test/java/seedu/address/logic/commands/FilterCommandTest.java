d7651739f7e85dadc2e9e7b313c8eea0c50ed427
don-ko <donaldkong1137@gmail.com>
2026-04-03 22:11:42 +0800

    test: Add tests for filter commands

--- src/test/java/seedu/address/logic/commands/FilterCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FilterCommandTest.java
@@ -34,7 +34,7 @@ import seedu.address.model.person.predicates.NameContainsPredicate;
 import seedu.address.model.person.predicates.PhoneContainsPredicate;
 import seedu.address.model.person.predicates.StatusEqualsPredicate;
 import seedu.address.model.person.predicates.TagContainsPredicate;
-import seedu.address.model.tag.Tag;
+import seedu.address.model.tag.TagFilter;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for FilterCommand.
@@ -57,7 +57,7 @@ public class FilterCommandTest {
      * Creates a FilterCommand with the given parameter filters and tag filters.
      */
     private FilterCommand createFilterCommand(Map<FilterType, List<String>> paramFilters,
-                                              List<Tag> tagFilters) {
+                                              List<TagFilter> tagFilters) {
         return new FilterCommand(paramFilters, tagFilters);
     }
 
@@ -325,7 +325,7 @@ public class FilterCommandTest {
 
     @Test
     public void execute_singleTagCriteria_filtersCorrectly() {
-        List<Tag> tagFilters = List.of(new Tag("job:manager"));
+        List<TagFilter> tagFilters = List.of(new TagFilter("job:manager"));
         FilterCommand command = createFilterCommand(new HashMap<>(), tagFilters);
 
         expectedModel.updateFilteredPersonList(new TagContainsPredicate(tagFilters));
@@ -335,7 +335,7 @@ public class FilterCommandTest {
     @Test
     public void execute_multipleTagFilters_filtersMatchingAll() {
         // TagContainsPredicate uses AND logic: person must have ALL specified tags
-        List<Tag> tagFilters = List.of(new Tag("rich:yes"), new Tag("job:manager"));
+        List<TagFilter> tagFilters = List.of(new TagFilter("rich:yes"), new TagFilter("job:manager"));
         FilterCommand command = createFilterCommand(new HashMap<>(), tagFilters);
 
         expectedModel.updateFilteredPersonList(new TagContainsPredicate(tagFilters));
@@ -345,7 +345,7 @@ public class FilterCommandTest {
 
     @Test
     public void execute_tagMatchesNoOne_emptyList() {
-        List<Tag> tagFilters = List.of(new Tag("nonexistent:tag"));
+        List<TagFilter> tagFilters = List.of(new TagFilter("nonexistent:tag"));
         FilterCommand command = createFilterCommand(new HashMap<>(), tagFilters);
 
         expectedModel.updateFilteredPersonList(new TagContainsPredicate(tagFilters));
@@ -354,7 +354,25 @@ public class FilterCommandTest {
 
     @Test
     public void execute_statusTagCriteria_filtersCorrectly() {
-        List<Tag> tagFilters = List.of(new Tag("rich:yes"));
+        List<TagFilter> tagFilters = List.of(new TagFilter("rich:yes"));
+        FilterCommand command = createFilterCommand(new HashMap<>(), tagFilters);
+
+        expectedModel.updateFilteredPersonList(new TagContainsPredicate(tagFilters));
+        assertFilterResult(command, model, List.of(BENSON));
+    }
+
+    @Test
+    public void execute_tagNameOnly_filtersByTagExistence() {
+        List<TagFilter> tagFilters = List.of(new TagFilter("job"));
+        FilterCommand command = createFilterCommand(new HashMap<>(), tagFilters);
+
+        expectedModel.updateFilteredPersonList(new TagContainsPredicate(tagFilters));
+        assertFilterResult(command, model, Arrays.asList(ALICE, BENSON, CARL));
+    }
+
+    @Test
+    public void execute_mixedTagNameAndValue_filtersByBoth() {
+        List<TagFilter> tagFilters = List.of(new TagFilter("job"), new TagFilter("rich:yes"));
         FilterCommand command = createFilterCommand(new HashMap<>(), tagFilters);
 
         expectedModel.updateFilteredPersonList(new TagContainsPredicate(tagFilters));
@@ -391,7 +409,7 @@ public class FilterCommandTest {
     public void execute_emailAndTagCriteria_filtersByBoth() {
         Map<FilterType, List<String>> criteria = new HashMap<>();
         criteria.put(FilterType.EMAIL, List.of("johnd@example.com"));
-        List<Tag> tagFilters = List.of(new Tag("job:manager"));
+        List<TagFilter> tagFilters = List.of(new TagFilter("job:manager"));
         FilterCommand command = createFilterCommand(criteria, tagFilters);
 
         expectedModel.updateFilteredPersonList(
@@ -419,7 +437,7 @@ public class FilterCommandTest {
         criteria.put(FilterType.NAME, List.of("Benson"));
         criteria.put(FilterType.PHONE, List.of("98765432"));
         criteria.put(FilterType.EMAIL, List.of("johnd@example.com"));
-        List<Tag> tagFilters = List.of(new Tag("job:manager"));
+        List<TagFilter> tagFilters = List.of(new TagFilter("job:manager"));
         FilterCommand command = createFilterCommand(criteria, tagFilters);
 
         expectedModel.updateFilteredPersonList(
@@ -459,7 +477,7 @@ public class FilterCommandTest {
     @Test
     public void equals_sameValues_returnsTrue() {
         Map<FilterType, List<String>> criteria = singleParamFilter(FilterType.NAME, "Alice");
-        List<Tag> tags = Collections.emptyList();
+        List<TagFilter> tags = Collections.emptyList();
 
         FilterCommand command1 = createFilterCommand(criteria, tags);
         FilterCommand command2 = createFilterCommand(criteria, tags);
@@ -497,9 +515,9 @@ public class FilterCommandTest {
     @Test
     public void equals_differentTagFilters_returnsFalse() {
         FilterCommand command1 = createFilterCommand(new HashMap<>(),
-                List.of(new Tag("job:manager")));
+                List.of(new TagFilter("job:manager")));
         FilterCommand command2 = createFilterCommand(new HashMap<>(),
-                List.of(new Tag("status:called")));
+                List.of(new TagFilter("status:called")));
 
         assertFalse(command1.equals(command2));
     }
@@ -530,7 +548,7 @@ public class FilterCommandTest {
     @Test
     public void toString_containsFilterInformation() {
         Map<FilterType, List<String>> criteria = singleParamFilter(FilterType.NAME, "Alice");
-        List<Tag> tags = List.of(new Tag("job:manager"));
+        List<TagFilter> tags = List.of(new TagFilter("job:manager"));
         FilterCommand command = createFilterCommand(criteria, tags);
         String result = command.toString();
 
